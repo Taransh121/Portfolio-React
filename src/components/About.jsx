8742ae20f9717a26c7ecef84b8a60d66e615f294
TARANSH CHELLANI <taranshchellani121@gmail.com>
2024-10-17 19:57:08 +0530

    updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
         </div>
         <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
           <div className='sm:text-right text-4xl font-bold'>
-            <p>Hi. I'm Taransh Chellani, a 3rd year BTech Student at VIT Bhopal University.</p>
+            <p>Hi. I'm Taransh Chellani, a 4th year BTech Student at VIT Bhopal University.</p>
           </div>
           <div>
             <p>I am Highly skilled and motivated software developer with a proven track record of designing and implementing innovative solutions to complex problems.
